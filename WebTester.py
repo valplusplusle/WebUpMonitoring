c1f9db8185351f7a147752205ac6a933ceea882b
admin <blank@h2808199.stratoserver.net>
2019-01-28 16:01:31 +0100

    add email send

--- WebTester.py
+++ WebTester.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# /usr/bin/python
+
 import urllib.request
 import time
+from smtplib import SMTP_SSL as SMTP
+from email.mime.text import MIMEText
+
+def send_email(subject, body, toaddr="valentin.kummert@webdesign-coburg.de"):
+	
+	with open('config.txt') as fileline:
+		configlines = fileline.readlines()
+	
+	fromaddr = configlines[0].rstrip()
+	msg = MIMEText(body, 'plain')
+	msg['To'] = toaddr
+	msg['Subject'] = subject
+	server = SMTP(configlines[1].rstrip())
+	server.login(fromaddr, configlines[2].rstrip())
+	server.sendmail(fromaddr, toaddr, msg.as_string())
+	server.quit()
 
 while True:
     with open('links.txt') as f:
@@ -15,14 +34,14 @@ while True:
                 response = \
                     urllib.request.urlopen(lines[counter]).getcode()
             except urllib.error.URLError:
-                response = "0"
+                response = '0'
 
             if response != 200:
-                logfile.write('OFFLINE;' + lines[counter])
+            	send_email("Web-Alert!", lines[counter]+"is Offline!")
+            	logfile.write('OFFLINE;' + lines[counter])
             else:
-                logfile.write('ONLINE;' + lines[counter])
+            	logfile.write('ONLINE;' + lines[counter])
             counter = counter + 1
     except ValueError:
-        print ("Error in the Links!")
-    time.sleep(5)
-    print ("ende")
+        print ('Error in the Links!')
+    time.sleep(60)
