c96b9b0f019266e251534ea65b77ae64eb8d778b
admin <blank@h2808199.stratoserver.net>
2019-01-31 13:54:26 +0100

    bugfixing

--- WebTester.py
+++ WebTester.py
@@ -31,33 +31,29 @@ while True:
     logfile = open('log.txt', 'w')
 
     with open('links.txt') as f:
-        lines = f.readlines()
-        alertmail.append('ONLINE')
-        
+        lines = f.readlines()        
     
     if firststart == 0:
         for x in range(0, 100):
-            alertmail.append('ONLINE')
+            alertmail.append(0)
         firststart = 1
 
     try:
         for val in lines:
             try:
-                response = \
-                    urllib.request.urlopen(lines[counter]).getcode()
+                response = urllib.request.urlopen(lines[counter]).getcode()
             except urllib.error.URLError:
                 response = '0'
-
+                
             if response != 200:
-                if alertmail[counter] == 'ONLINE':
-                    send_email('Web-Alert!', lines[counter]
-                               + 'is Offline!')
-                    alertmail[counter] = 'OFFLINE'
+                if alertmail[counter] == 3:
+                    send_email('Web-Alert!', lines[counter] + 'is Offline!')
+                alertmail[counter] = alertmail[counter] + 1 
                 logfile.write('OFFLINE;' + lines[counter])
             else:
                 logfile.write('ONLINE;' + lines[counter])
-                if alertmail[counter] == 'OFFLINE':
-                    alertmail[counter] = 'ONLINE'
+                if alertmail[counter] > 0:
+                    alertmail[counter] = 0
             counter = counter + 1
     except ValueError:
         print ('Error in the Links!')
